bdf7fd42c212e4f7a47af8f3074654d803d901bd
leonard0bw <bajanaleonardo.97@gmail.com>
2024-04-12 20:57:32 -0500

    Prueba

--- Back_Vinculacion_Fema/Back_Vinculacion_Fema/Controllers/AuthController.cs
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Back_Vinculacion_Fema.Controllers
 {
+    //Prueba
     [ApiController]
     [Route("[controller]")]
     public class AuthController : ControllerBase
