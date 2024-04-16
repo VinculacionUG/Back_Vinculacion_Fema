4444d368283ec7b5a6f831fbf34c621e44e65eb5
Alfredo López <alfredo.lg1726@gmail.com>
2024-04-16 11:34:03 -0500

    Se agregó un método privado para cifrar la password.

--- Back_Vinculacion_Fema/Back_Vinculacion_Fema/Controllers/AuthController.cs
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
-﻿using Back_Vinculacion_Fema.CRUD;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Back_Vinculacion_Fema.CRUD;
 using Back_Vinculacion_Fema.Models.DbModels;
 using Back_Vinculacion_Fema.Models.RequestModels;
 using Back_Vinculacion_Fema.Models.Utilidades;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace Back_Vinculacion_Fema.Controllers
 {
@@ -20,7 +23,7 @@ namespace Back_Vinculacion_Fema.Controllers
         [HttpPost("login")]
         public IActionResult Authenticate(UserLoginRequest credentials)
         {
-            var encryptedPassword = credentials.Password; //Debe consumir el metodo de cifrado
+            var encryptedPassword = EncryptPassword(credentials.Password); ; //Debe consumir el metodo de cifrado
 
             User usuarioLogic = new User(_contexto);
             var usuario = usuarioLogic.GetUsuarioLogin(credentials.Nombre, encryptedPassword);
@@ -31,6 +34,23 @@ namespace Back_Vinculacion_Fema.Controllers
             }
 
             return Ok(Token.GenerarToken(usuario.UserName));
-        }   
+        }
+
+        private string EncryptPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+
     }
 }
