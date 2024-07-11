505f874b87c8151dc9377152885144d0e2f5f5c4
VinculacionUG <alexandermatailo@hotmail.com>
2024-07-10 23:11:40 -0500

    eliminacion Espacio

--- Back_Vinculacion_Fema/Back_Vinculacion_Fema/Interface/IFemaTres.cs
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Interface/IFemaTres.cs
@@ -6,7 +6,6 @@ namespace Back_Vinculacion_Fema.Interface
     {
         //public Task<IEnumerable<TipoEdificacion>> tipoEdificacions();
         public Dictionary<string,Object> tipoEdificacions();
-
         public Task<IEnumerable<SubtipoEdificacion>> SubtipoEdificacions(int idTipoEdificacion);
 
         public Task<IEnumerable<PuntuacionMatriz>> PuntuacionMatrizs(int codSubTipoEdificacion);
