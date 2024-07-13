b51e14b5ee00356900c3241f48d786fb0a6f2ab0
Alfredo López <alfredo.lg1726@gmail.com>
2024-07-13 01:43:19 -0500

    Endpoint para extraer un formulario fema meidnate el id, y un endpoint para actualizar un formulario mediante de su id

---
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Models/DTOs/FemaPuntuacionDto.cs
@@ -0,0 +1,14 @@
+﻿using Back_Vinculacion_Fema.Models.DbModels;
+using Back_Vinculacion_Fema.Models.DTOs;
+
+namespace Back_Vinculacion_Fema.Models.DTOs
+{
+    public class FemaPuntuacionDto
+    {
+        public short CodPuntuacionMatriz { get; set; }
+        public decimal ResultadoFinal { get; set; }
+        public bool EsEst { get; set; }
+        public bool EsDnk { get; set; }
+    }
+}
+
