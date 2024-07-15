f4a25df1760ef159083465f56d52d5d14e16ce7c
VinculacionUG <alexandermatailo@hotmail.com>
2024-07-15 06:26:19 -0500

    integracion de nuevas clases

--- Back_Vinculacion_Fema/Back_Vinculacion_Fema/Models/DTOs/FemaOcupacionDto.cs
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Models/DTOs/FemaOcupacionDto.cs
@@ -6,4 +6,29 @@
         public short CodOcupacion { get; set; }
         public short CodTipoOcupacion { get; set; }
     }
+
+    public class FemaOcupacionDtoResponse
+    {
+        public int CodOcupacionSecuencia { get; set; }
+        public int CodFema { get; set; }
+        public short CodOcupacion { get; set; }
+        public short CodTipoOcupacion { get; set; }
+        public bool EstadoFO { get; set; }
+
+        public FemaTipoOcupacionDtoResponse femaTipoOc { get; set; }
+
+        public OcupacionDtoResponse ocupacionDto { get; set; }
+    }
+    public class FemaTipoOcupacionDtoResponse
+    {
+        public short CodTipoOcupacion { get; set; }
+        public string DescripcionTO { get; set; } = null!;
+        public bool EstadoTO { get; set; }
+    }
+    public class OcupacionDtoResponse
+    {
+        public short CodOcupacion { get; set; }
+        public string DescripcionO { get; set; } = null!;
+        public bool EstadoO { get; set; }
+    }
 }
--- Back_Vinculacion_Fema/Back_Vinculacion_Fema/Models/DTOs/FemaPuntuacionDto.cs
+++ Back_Vinculacion_Fema/Back_Vinculacion_Fema/Models/DTOs/FemaPuntuacionDto.cs
@@ -10,5 +10,49 @@ namespace Back_Vinculacion_Fema.Models.DTOs
         public bool EsEst { get; set; }
         public bool EsDnk { get; set; }
     }
+    public class FemaPuntuacionDtoResponse
+    {
+        public long CodPuntuacionSec { get; set; }
+        public int CodFema { get; set; }
+        public short CodPuntuacionMatriz { get; set; }
+        public decimal ResultadoFinal { get; set; }
+        public bool EsEst { get; set; }
+        public bool Estado { get; set; }
+        public bool EsDnk { get; set; }
+
+        public PuntuacionMatriz puntuacion { get; set; }
+    }
+    public class PuntuacionMatrizDto
+    {
+        public short CodPuntuacionMatriz { get; set; }
+        public decimal ResultadoFinal { get; set; }
+        public bool EsEst { get; set; }
+        public bool EsDnk { get; set; }
+
+        public SubtipoEdificacion subtipoEd { get; set; }
+        public TipoPuntuacion tipoPuntuacion { get; set; }
+    }
+    public class SubtipoEdificacion
+    {
+        public short CodSubtipoEdificacion { get; set; }
+        public short CodTipoEdificacion { get; set; }
+        public string DescripcionSE { get; set; } = null!;
+        public bool EstadoSE { get; set; }
+
+        public TipoEdificacionDto tipoEdificacion { get; set; }
+
+    }
+    public class TipoEdificacionDto
+    {
+        public short CodTipoEdificacion { get; set; }
+        public string DescripcionTE { get; set; } = null!;
+        public bool EstadoTE { get; set; }
+    }
+    public class TipoPuntuacion
+    {
+        public short CodTipoPuntuacion { get; set; }
+        public string DescripcionTP { get; set; } = null!;
+        public bool EstadoTP { get; set; }
+    }
 }
 
